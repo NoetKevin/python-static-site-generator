86e4a547e1a7a44f1ea0887fb5f7dc04ef9df9cd
NoetKevin <kevin.noet@soprasteria.com>
2022-08-10 14:43:17 +0200

    add argument type method parse change the argument pass for read function Modify the extension '.html' by ".html" Add the argument self at the function copy

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -16,19 +16,19 @@ class Parser:
     def valid_extension(self, extension):
         return extension in self.extensions
 
-    def parse(self, path, source, dest):
+    def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
 
     def read(self, path):
-        with open(path, 'r') as lines:
-            return lines.read()
+        with open(path, 'r') as file:
+            return file.read()
 
-    def write(self, path, dest, content, ext='.html'):
+    def write(self, path, dest, content, ext=".html"):
         full_path = self.dest / path.with_suffix(ext)
         with open(full_path, 'w') as file:
             file.write(content)
 
-    def copy(path, source, dest):
+    def copy(self, path, source, dest):
         shutil.copy2(path, self.dest / path.relative_to(self.source))
 
 
