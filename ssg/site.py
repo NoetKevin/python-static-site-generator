dcec2f501a633884530b2d5b8e201590b6fe8c23
NoetKevin <kevin.noet@soprasteria.com>
2022-08-10 16:53:09 +0200

    Change message output of method error

--- ssg/site.py
+++ ssg/site.py
@@ -35,7 +35,7 @@ class Site:
             parser.parse(path, self.source, self.dest)
         else:
             self.error("No parser for the {} extension, file skipped!".format(path.suffix))
-    
+
     @staticmethod
     def error(self, message):
-        sys.stderr.write("\x1b[1;31m{}\n]".format(message))
+        sys.stderr.write("\x1b[1;31m{}\n".format(message))
