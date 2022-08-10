6f1e9197fd65eac0a6def0c84308455fff22230d
NoetKevin <kevin.noet@soprasteria.com>
2022-08-10 16:44:20 +0200

    Parser Subclasses

--- ssg.py
+++ ssg.py
@@ -1,9 +1,11 @@
 import typer
+import ssg.parsers
+
+
 from ssg.site import Site
-import ssg.parsers 
 
 def main(source="content", dest="dist", parsers=None):
-    config = {"source": source, "dest": dest, "parsers": [ssg.parsers.ResourceParser()]}
+    config = {"source": source, "dest": dest, "parsers": [ssg.parsers.ResourceParser(), ssg.parsers.MarkdownParser(), ssg.parsers.ReStructuredTextParser()]}
     Site(**config).build()
 
 typer.run(main)
